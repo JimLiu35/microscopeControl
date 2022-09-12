82004e727c0d98d0efdfee3e10ca95a6ea76a494
danielyan <danielyan0602@gmail.com>
2022-09-11 22:47:55 -0400

    Sample Laplacian Test

---
+++ Samples/LaplacianTest.py
@@ -0,0 +1,11 @@
+import cv2
+
+filename = "d.image" #images
+
+img = cv2.imread(filename,cv2.IMREAD_GRAYSCALE) # Read image via grayscale
+laplacian_var = cv2.Laplacian(img, cv2.CV_64F).var() # Read in laplacian value
+
+print(laplacian_var)
+
+cv2.imshow("Img", img)
+cv2.waitKey(0)
