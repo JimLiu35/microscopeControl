a33707a1659dfa1fe4da197380fee5cf175ced14
danielyan <danielyan0602@gmail.com>
2022-09-19 14:15:12 -0400

    Added Image

--- Samples/LaplacianTest.py
+++ Samples/LaplacianTest.py
@@ -1,6 +1,6 @@
 import cv2
 
-filename = "d.image" #images
+filename = "d.jpg" #images
 
 img = cv2.imread(filename,cv2.IMREAD_GRAYSCALE) # Read image via grayscale
 laplacian_var = cv2.Laplacian(img, cv2.CV_64F).var() # Read in laplacian value
